a8f49714dfbac82668bc12c227fd55d2a7aa8a7f
alternadiva <85565743+alternadiva@users.noreply.github.com>
2021-11-24 15:36:39 +0100

    create html css js files, DOM

---
+++ script.js
@@ -0,0 +1,8 @@
+// collect data from HTML
+const card = document.getElementsByClassName("cards");
+const cards = [...card];
+
+const result = document.getElementById("result");
+
+console.log(cards);
+
