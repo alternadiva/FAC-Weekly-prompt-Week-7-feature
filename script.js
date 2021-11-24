e3584ce7b4d07349632c3f25d7217fe3cfca94a4
alternadiva <85565743+alternadiva@users.noreply.github.com>
2021-11-24 18:23:42 +0100

    add shuffle function to randomize the order of the cards

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 // collect data from HTML
+const table = document.getElementById("table");
 const card = document.getElementsByClassName("cards");
-const cards = [...card];
+const cards = table.children;
 
 const result = document.getElementById("result");
 
-console.log(cards);
+let fragment = document.createDocumentFragment(); //This method creates the document fragment, then append the elements of the document to the document fragment and make the changes according to the need. It is a safe method and thus prevents destroying of the DOM structure.
 
+function shuffleCards() {
+    while (cards.length) {
+        fragment.appendChild(cards[Math.floor(Math.random() * cards.length)]);
+    }
+    table.appendChild(fragment);
+    return cards;
+}
+
+window.addEventListener("load", shuffleCards)
+
+console.log(cards)
