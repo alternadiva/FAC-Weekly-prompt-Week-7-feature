36085b1d5b6ba15d635917aadd66a8add6c65aaf
alternadiva <85565743+alternadiva@users.noreply.github.com>
2021-11-28 20:43:35 +0100

    add flip effect on cards

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
 // collect data from HTML
 const table = document.getElementById("table");
-const card = document.getElementsByClassName("cards");
-const cards = table.children;
+const cards = document.querySelectorAll(".cards");
+const cardOrder = table.children;
 
 const result = document.getElementById("result");
+
+// shuffle cards on page load - add function and add event listener
 
 let fragment = document.createDocumentFragment(); //This method creates the document fragment, then append the elements of the document to the document fragment and make the changes according to the need. It is a safe method and thus prevents destroying of the DOM structure.
 
 function shuffleCards() {
-    while (cards.length) {
-        fragment.appendChild(cards[Math.floor(Math.random() * cards.length)]);
+    while (cardOrder.length) {
+        fragment.appendChild(cardOrder[Math.floor(Math.random() * cardOrder.length)]);
     }
     table.appendChild(fragment);
-    return cards;
+    return cardOrder;
 }
 
 window.addEventListener("load", shuffleCards)
 
-console.log(cards)
+//flip cards on click - giving class attribute to the clicked cards and setting the css effect
+
+function flipCards() {
+    this.classList.toggle('flip');
+}
+
+cards.forEach(card => card.addEventListener('click', flipCards))
+
+//find matching cards
