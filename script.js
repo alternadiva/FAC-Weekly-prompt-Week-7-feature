9c31735dae2bb710036c707f4fe83ed81faa07b2
alternadiva <85565743+alternadiva@users.noreply.github.com>
2021-11-29 17:32:38 +0100

    setup data-id, find matches, unmatched remove flip

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const cardOrder = table.children;
 
 const result = document.getElementById("result");
 
+let isItFlipped;
+let flippedCardOne;
+let flippedCardTwo;
+
 // shuffle cards on page load - add function and add event listener
 
 let fragment = document.createDocumentFragment(); //This method creates the document fragment, then append the elements of the document to the document fragment and make the changes according to the need. It is a safe method and thus prevents destroying of the DOM structure.
@@ -22,9 +26,45 @@ window.addEventListener("load", shuffleCards)
 //flip cards on click - giving class attribute to the clicked cards and setting the css effect
 
 function flipCards() {
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+
+    if (isItFlipped === undefined) {
+        isItFlipped = true;
+        flippedCardOne = this;
+    }
+    else {
+        flippedCardTwo = this;
+    }
+    isItAMatch()
 }
 
 cards.forEach(card => card.addEventListener('click', flipCards))
 
-//find matching cards
+//find matching cards - keep the pairs front-side and turn back the unmatched cards
+
+function isItAMatch() {
+    isItFlipped = false;
+
+    let cardOneData = flippedCardOne.dataset.id;
+    let cardTwoData = flippedCardTwo.dataset.id;
+
+    if (cardOneData === cardTwoData) {
+        disableCards();
+    }
+    else {
+        unflipCards()
+    }
+}
+
+function disableCards() {
+    flippedCardOne.removeEventListener('click', flipCards);
+    flippedCardTwo.removeEventListener('click', flipCards);
+}
+
+function unflipCards() {
+    setTimeout(function () {
+        flippedCardOne.classList.remove('flip');
+        flippedCardTwo.classList.remove('flip');
+    }
+    , 1200);
+}
